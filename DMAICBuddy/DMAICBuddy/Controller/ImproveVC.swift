ff361a74a8adc31f038cc0d41d61d5920fcffad0
DevOps Tinman <devops.tinman@gmail.com>
2019-02-07 08:24:28 -0600

    added analyze

--- DMAICBuddy/DMAICBuddy/Controller/ImproveVC.swift
+++ DMAICBuddy/DMAICBuddy/Controller/ImproveVC.swift
@@ -1,21 +1,27 @@
 //
-//  IntroVC.swift
+//  ImproveVC.swift
 //  DMAICBuddy
 //
-//  Created by Allbee, Eamon on 1/23/19.
+//  Created by Allbee, Eamon on 2/6/19.
 //  Copyright © 2019 Allbee, Eamon. All rights reserved.
 //
 
 import UIKit
 
-class IntroVC: UIViewController {
+class ImproveVC: UIViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func backBtnPressed(_ sender: Any) {
+         dismiss(animated: true, completion: nil)
+    }
     
-    
+
 }
